583ae4e10aaf02a0d930d649cdaf6f5f48fed734
AndreaCollura <andrea.collur@gmail.com>
2023-03-09 19:38:02 +0100

    added ui details + js features

--- js/script.js
+++ js/script.js
@@ -14,7 +14,15 @@ const buttonCal = document.getElementById('btncal');
 
 const buttonNew = document.getElementById('btnnew');
 
-const ticketFinal = document.querySelector('ticketfinal');
+const totPrice = document.querySelector('.totprice');
+
+const discountType = document.querySelector('.discount');
+
+const ticketBox = document.querySelector('.ticket-box');
+
+const passName = document.querySelector('.pass-name');
+
+const rndNumber = document.querySelector('.rndnum');
 
 const juniorSale = 0.20;
 
@@ -35,20 +43,53 @@ buttonCal.addEventListener('click', function () {
 
     let price = (km * kmPrice) ; 
 
+    let randomNum = getRndNumber(500,99999);
+
+    passName.innerHTML = `<p> ${nameImp.value}</p>`;
+
+    ticketBox.classList.remove('d-none');
+
+
+
     switch (true) {
-        case (age < 18):
+        case (age < 18 && age > 0):
             finalPrice = (price - (price * juniorSale)).toFixed(2);
+            discountType.innerHTML = `<p>Sconto Junior</p>`;
+            totPrice.style.color = 'green';
+            discountType.style.color = 'green';
+            rndNumber.innerHTML = `<p>${randomNum}</p>`;
+            totPrice.innerHTML = `<p>${finalPrice}€</p>`;
             break;
 
         case (age >= 65):
             finalPrice = (price - (price * seniorSale)).toFixed(2);
+            discountType.innerHTML = `<p>Sconto Senior</p>`;
+            discountType.style.color = 'green';
+            totPrice.style.color = 'green';
+            rndNumber.innerHTML = `<p>${randomNum}</p>`;
+            totPrice.innerHTML = `<p>${finalPrice}€</p>`;
             break;
         
         default : 
             
-            finalPrice = price;
-            
+            finalPrice = price.toFixed(2);
+            discountType.innerHTML = `<p>Nessuno Sconto</p>`;
+            rndNumber.innerHTML = `<p>${randomNum}</p>`;
+            totPrice.innerHTML = `<p>${finalPrice}€</p>`;
+
     }
+
+
+    buttonNew.addEventListener('click', function(){
+
+        kmImp.value = '';
+        ageImp.value = '';
+        nameImp.value = '';
+        ticketBox.classList.add('d-none');
+
+
+
+    })
    
 
     console.log(finalPrice);
